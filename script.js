799577e471d36503cd2fd8ab070e3c43d4189ea2
Obiefuna Kingsley <kingsleyobiefuna01@gmail.com>
2024-07-30 23:02:14 +0200

    bundled the style together with the script

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-let drop_btns = document.querySelectorAll(".drop-btn");
+function dropDown() {
+  let drop_btns = document.querySelectorAll(".drop-btn");
 
-drop_btns.forEach((btn) => {
-  btn.addEventListener("click", toggleVisibility);
-});
+  drop_btns.forEach((btn) => {
+    btn.addEventListener("click", toggleVisibility);
+  });
 
-function toggleVisibility(event) {
-  // get the drop-down list node
-  let toggled_node = event.target.nextElementSibling;
+  function toggleVisibility(event) {
+    // get the drop-down list node
+    let toggled_node = event.target.nextElementSibling;
 
-  toggled_node.classList.toggle("visible");
+    toggled_node.classList.toggle("visible");
 
-  //   hide all child drop downs
-  if (!toggled_node.classList.contains("visible")) {
-    let inner_drop_downs = toggled_node.querySelectorAll(".drop-down");
+    //   hide all child drop downs
+    if (!toggled_node.classList.contains("visible")) {
+      let inner_drop_downs = toggled_node.querySelectorAll(".drop-down");
 
-    inner_drop_downs.forEach((drop_down) => {
-      drop_down.classList.remove("visible");
-    });
+      inner_drop_downs.forEach((drop_down) => {
+        drop_down.classList.remove("visible");
+      });
+    }
   }
 }
